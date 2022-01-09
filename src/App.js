e87fd1fe6e9b336f71ad8c582740586d1fa78c5d
Anthony Del Russo <acdelrusso@gmail.com>
2022-01-09 12:00:38 -0500

    add mock backend server to fetch data from

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react'
+import NewNote from './components/NewNote'
+import Notes from './components/Note'
+import VisibilityFilter from './components/VisibilityFilter'
+import noteService from './services/notes'
+import { initializeNotes } from './reducers/noteReducer'
+import { useDispatch } from 'react-redux'
 
-function App() {
+const App = () => {
+    const dispatch = useDispatch()
+    useEffect(() => {
+        noteService.getAll().then(notes => {
+            dispatch(initializeNotes(notes))
+        })
+        
+    }, [dispatch])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <NewNote />
+    <VisibilityFilter />
+    <Notes />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
